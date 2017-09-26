fe251f9e5ecfd84ff0812ae1f13ed41d09e3692a
NarvalNudiste <guillaume.noguera@gmail.com>
2017-09-26 11:38:37 +0200

    removing some comments

--- testData.py
+++ testData.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy import genfromtxt
 import warnings
 
-DEBUG = False
+debug_mode = False
 
 def predict(clf, eda, hr):
 	print("Prediction -> hr = ", hr, " / eda = ", eda, " || result = ", clf.predict([hr, eda]))
@@ -12,18 +12,13 @@ def main():
 	dl = dataLoader('test_data/HR.csv', 'test_data/STRESS.csv', 'test_data/EDA.csv')
 
 	clf = svm.SVC(gamma = 0.001, C=100)
-	clf.fit(dl.getValues()[:-1], dl.getTargetValues()[:-1])
+	clf.fit(dl.X[:-1], dl.Y[:-1])
 
 	#We now can try to predict stress target value acoording to various inputs
 	#EDA values range from 0.0 to 1.000
 	#HR values range from 50 to 70
 	
 	predict(clf, 0.16, 50)
-	#predict(clf, 0.22, 50)
-	
-	print(dl.X)
-	print(dl.Y)
-	
 	
 
 
@@ -43,26 +38,16 @@ class dataLoader():
 		
 
 		#We declare a 2-dimensional array to store values
-		#self.X = list()
 		self.X = np.empty([len(self.stress_data), 2])
 		
 		for i in range(len(self.stress_data)):
-			#self.X.append([self.hr_data[i], self.eda_data[i]])
 			self.X[i] = ([self.hr_data[i], self.eda_data[i]])
 		
+		#TODO : fix 1d scikit deprecation issue
 		self.Y = np.array(self.stress_data)
-		#self.X = np.array(self.X).reshape(-1, 1)
 		
-		'''QoL getter
-	'''
-	def getValues(self):
-		return self.X
-	'''QoL getter
-	'''
-	def getTargetValues(self):
-		return self.Y
 
-if DEBUG is True:
+if debug_mode is True:
 	with warnings.catch_warnings():
 		warnings.simplefilter("error")
 		main()
