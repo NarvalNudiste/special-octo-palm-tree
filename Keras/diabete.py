96175364daea5723319ce4cb9e468ec8a01f15e4
Guillaume Noguera <guillaume.noguera@gmail.com>
2017-10-30 18:03:53 +0100

    additional testing

--- Keras/diabete.py
+++ Keras/diabete.py
@@ -7,29 +7,41 @@ numpy.random.seed(1337)
 
 dataset = numpy.loadtxt("data/pima-indians-diabetes.data", delimiter=",")
 # split into input (X) and output (Y) variables
+
 X = dataset[:,0:8]
 Y = dataset[:,8]
 
-callbacks = list()
-tbCallback = TensorBoard(log_dir='logs', histogram_freq=0,
-          write_graph=True, write_images=True)
-callbacks.append(tbCallback)
+X_train = X[:576]
+X_test = X[576:]
+
+Y_train = Y[:576]
+Y_test = Y[576:]
+
+#callbacks = list()
+#tbCallback = TensorBoard(log_dir='logs', histogram_freq=0,
+#          write_graph=True, write_images=True)
+#callbacks.append(tbCallback)
 
 m = Sequential()
-m.add(Dense(16, input_dim=8, activation = 'relu'))
-m.add(Dropout(16))
-for i in range(8):
-  m.add(Dense(16, activation = 'relu'))
-  m.add(Dropout(16))
+m.add(Dense(8, input_dim=8, activation = 'relu'))
+m.add(Dropout(8))
+m.add(Dense(8, input_dim=8, activation = 'relu'))
+m.add(Dropout(8))
+m.add(Dense(8, input_dim=8, activation = 'relu'))
+m.add(Dropout(8))
+
 m.add(Dense(1, activation="sigmoid"))
 m.summary()
 
 m.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 input("- Model compiled, press enter to train ")
-m.fit(X, Y, epochs=10000, batch_size=128, callbacks = callbacks)
-
+#m.fit(X, Y, epochs=10000, batch_size=128, callbacks = callbacks)
+m.fit(X_train, Y_train, epochs=50000, batch_size=128, verbose=0)
 
 # evaluate the model
-scores = m.evaluate(X, Y)
-print("\n%s: %.2f%%" % (m.metrics_names[1], scores[1]*100))
+#scores = m.evaluate(X, Y)
+scoresT = m.evaluate(X_train, Y_train)
+print("Training scores : \n%s: %.2f%%" % (m.metrics_names[1], scoresT[1]*100))
+scores = m.evaluate(X_test, Y_test)
+print("Testing scores : \n%s: %.2f%%" % (m.metrics_names[1], scores[1]*100))
